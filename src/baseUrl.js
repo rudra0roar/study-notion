decef85244d446bbade6262ac2409df5fa61a43c
Mukesh Kumar <mukeshkumar117733@gmail.com>
2024-03-20 23:39:02 +0530

    Url and Build

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://studynotion-backend-fr3y.onrender.com/api/v1"
+// export const BASE_URL = "https://studynotion-backend-fr3y.onrender.com/api/v1"
+export const BASE_URL = "https://study-notion-2pev.onrender.com/api/v1"
